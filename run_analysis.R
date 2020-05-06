60f43dab2bcee41f3717a8f158fd1cc6560b30a4
NamanGupta0611 <64494385+NamanGupta0611@users.noreply.github.com>
2020-05-06 19:37:31 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,19 +5,19 @@ library(dplyr)
 file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(file_url,"AssignData.zip",method = "curl")
 # Check if folder exist.
-if(!file_exists("UCI HAR Data"){
+if(!file.exists("UCI HAR Dataset"){
   unzip("AssignData.zip")
 }
    
 ##Assigning all the data frames
-features <- read.table("UCI HAR Data/features.txt", col.names = c("n","functions"))
-activities <- read.table("UCI HAR Data/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("UCI HAR Data/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("UCI HAR Data/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("UCI HAR Data/test/y_test.txt", col.names = "code")
-subject_train <- read.table("UCI HAR Data/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Data/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("UCI HAR Data/train/y_train.txt", col.names = "code")
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 # Merging the test and training dataset into one data frame
 X <- rbind(x_train,x_test)
@@ -26,7 +26,7 @@ Subject <- rbind(subject_train,subject_test)
 Merged_data <- cbind(Subject,X,Y)
 
 # Extracts only the measurements on the mean and standard deviation for each measurement
-TidySet <- Merged_data %>% select(subject,code,contains("mean"),contains("std"))
+TidyData <- Merged_data %>% select(subject,code,contains("mean"),contains("std"))
 
 # Uses descriptive activity names to name the activities in the data set
 TidyData$code <- activities[TidyData$code,2]
